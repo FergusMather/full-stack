ca79a4b0d56a4419547f15290a7b261e73dc522b
FergusMather <mather.fergus@gmail.com>
2022-06-09 20:29:17 +0000

    DELETE implementation somethjing not working versions different

--- src/global.d.ts
+++ src/global.d.ts
@@ -10,6 +10,7 @@
 //}
 
 type Todo = {
+	uid: String;
 	created_at: Date;
 	text: String;
 	done: boolean;
--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -1,29 +1,18 @@
 import type { RequestHandler } from "@sveltejs/kit";
+import { api } from "./_api";
 
-//TODO: persist in database
-let todos: Todo[] = [];
-
-export const get: RequestHandler = () => {
-    return {
-      status: 200,
-      body: todos
-    }
+export const get: RequestHandler = (request) => {
+    return api(request);
   }
 
 export const post: RequestHandler = async ({ request }) => {
-    const formData = await request.formData();   
-    todos.push({
+    const formData = await request.formData(); 
+    return api(request, {
+        uid: Date.now.toString(), // TODO replace with the UID from database
         created_at: new Date(),
         text: formData.get("text") as String,
         done: false
-    })
-  
-    return {
-        status: 303,
-        body: formData.get("text") as string,
-        headers:{
-          location: "/"
-        }           
-    }
+    })  
+   
   }
  
